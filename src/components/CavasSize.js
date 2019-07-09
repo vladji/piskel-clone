aa1eda25958af64c8ac087922455f7c5cf1be142
vladji <vladjislav@gmail.com>
2019-07-09 15:59:08 +0300

    fixup

--- src/components/CavasSize.js
+++ src/components/CavasSize.js
@@ -5,6 +5,7 @@ export default class CanvasSize {
     this.wrapSizeBtn = document.querySelector('.wrap_size-btn');
     this.defaultSizeBtn = document.querySelector('.large-canvas').className;
     this.stateBuffer = [this.defaultSizeBtn];
+    this.redrawStateBuffer = [];
     this.logic();
   }
 
@@ -17,20 +18,9 @@ export default class CanvasSize {
 
     this.activeSize(sizeBtn);
     this.chooseSize();
-  }
 
-  activeSize(sizeBtn) {
-    this.stateBuffer.push(sizeBtn);
-
-    const prevState = this.stateBuffer.shift();
-    const prevSize = document.querySelector(`.${prevState}`);
-    prevSize.style.border = '2px solid #a7a79d';
-
-    const currentState = this.stateBuffer[0];
-    const currentSize = document.querySelector(`.${currentState}`);
-    currentSize.style.border = '2px solid #ffed15';
-
-    this.recalcThick(currentSize);
+    const startSizeBtn = document.querySelector('.large-canvas');
+    this.recalcThick(startSizeBtn);
   }
 
   chooseSize() {
@@ -40,6 +30,22 @@ export default class CanvasSize {
         this.activeSize(target);
       }
     });
+  }
+
+  activeSize(sizeBtn) {
+    this.stateBuffer.push(sizeBtn);
+    this.redrawStateBuffer = this.stateBuffer.slice(0, this.stateBuffer.length);
+
+    const prevState = this.stateBuffer.shift();
+    const prevSize = document.querySelector(`.${prevState}`);
+    prevSize.style.border = '2px solid #a7a79d';
+
+    const currentState = this.stateBuffer[0];
+    const currentSize = document.querySelector(`.${currentState}`);
+    currentSize.style.border = '2px solid #ffed15';
+
+
+    if (prevState !== currentState) this.recalcThick(currentSize);
   }
 
   recalcThick(currentSize) {
@@ -53,7 +59,7 @@ export default class CanvasSize {
     const currentThickBtn = document.querySelector('.thickness-tool li[style*="rgb(255, 237, 21)"]');
     window.prepareCanvas(null, currentThickBtn.dataset.thick);
 
-    this.redrawCanvas();
+    // this.redrawCanvas();
   }
 
   redrawCanvas() {
@@ -62,17 +68,20 @@ export default class CanvasSize {
     tempCanvas.width = canvas.width;
     tempCanvas.height = canvas.height;
 
-    const tmpCanvCtx = tempCanvas.getContext('2d');
-    const canvCtx = canvas.getContext('2d');
-    // canvCtx.save();
-    tmpCanvCtx.scale(2, 2);
-    tmpCanvCtx.drawImage(canvas, 0, 0);
-    tmpCanvCtx.scale(2, 2);
-    // canvCtx.restore();
-    // eslint-disable-next-line max-len
-    // tmpCanvCtx.drawImage(canvas, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width * 4, canvas.height * 4);
+    // const wrap = document.querySelector('.canvas-wrapper');
+    // wrap.appendChild(tempCanvas);
 
-    canvCtx.clearRect(0, 0, canvas.width, canvas.height);
-    canvCtx.drawImage(tempCanvas, 0, 0);
+    const canvCtx = canvas.getContext('2d');
+    let canvData = canvCtx.getImageData(0, 0, canvas.width, canvas.height);
+    canvCtx.save();
+    canvCtx.scale(2, 2);
+    canvCtx.putImageData(canvData, 0, 0);
+    canvData = canvCtx.getImageData(0, 0, canvas.width, canvas.height);
+
+    const tmpCanvCtx = tempCanvas.getContext('2d');
+    tmpCanvCtx.scale(2, 2);
+    tmpCanvCtx.putImageData(canvData, 0, 0);
+
+    // canvCtx.restore();
   }
 }
